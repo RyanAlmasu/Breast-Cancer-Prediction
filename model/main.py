53002292545ed8980c91f0b46cdefb3f1d5408eb
RyanAlmasu <ryanalimasud3@gmail.com>
2024-01-05 15:27:23 +0700

    Update

--- model/main.py
+++ model/main.py
@@ -5,45 +5,53 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 import pickle5 as pickle
 
-def train_and_save_model(data): 
-    X = data.drop(['diagnosis'], axis=1)
-    y = data['diagnosis']
-    
-    # scale the data
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-    
-    # split the data
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_scaled, y, test_size=0.2, random_state=42
-    )
-    
-    # train the model
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
-    
-    # test model
-    y_pred = model.predict(X_test)
-    print('Accuracy of the model: ', accuracy_score(y_test, y_pred))
-    print("Classification report: \n", classification_report(y_test, y_pred))
-    
-    return model, scaler
 
-def load_clean_data():
-    data = pd.read_csv("data/data.csv")
-    data = data.drop(['Unnamed: 32', 'id'], axis=1)
-    data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
-    return data
+def create_model(data): 
+  X = data.drop(['diagnosis'], axis=1)
+  y = data['diagnosis']
+  
+  # scale the data
+  scaler = StandardScaler()
+  X = scaler.fit_transform(X)
+  
+  # split the data
+  X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+  )
+  
+  # train the model
+  model = LogisticRegression()
+  model.fit(X_train, y_train)
+  
+  # test model
+  y_pred = model.predict(X_test)
+  print('Accuracy of our model: ', accuracy_score(y_test, y_pred))
+  print("Classification report: \n", classification_report(y_test, y_pred))
+  
+  return model, scaler
+
+
+def get_clean_data():
+  data = pd.read_csv("data/data.csv")
+  
+  data = data.drop(['Unnamed: 32', 'id'], axis=1)
+  
+  data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
+  
+  return data
+
 
 def main():
-    data = load_clean_data()
-    trained_model, data_scaler = train_and_save_model(data)
+  data = get_clean_data()
 
-    with open('model/trained_model.pkl', 'wb') as model_file:
-        pickle.dump(trained_model, model_file)
+  model, scaler = create_model(data)
+
+  with open('model/model.pkl', 'wb') as f:
+    pickle.dump(model, f)
     
-    with open('model/data_scaler.pkl', 'wb') as scaler_file:
-        pickle.dump(data_scaler, scaler_file)
+  with open('model/scaler.pkl', 'wb') as f:
+    pickle.dump(scaler, f)
+  
 
 if __name__ == '__main__':
-    main()
+  main()
